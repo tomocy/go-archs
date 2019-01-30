544a8b0ac78f0561f18a642a144841cfbe0f9541
tomocy <tomocy.dev@gmail.com>
2019-01-30 13:44:56 +0900

    compile usecase errors to one file

--- usecase/usecase_error.go
+++ usecase/usecase_error.go
@@ -53,3 +53,27 @@ func (e incorrectCredentialError) Error() string {
 
 func (e incorrectCredentialError) incorrectCredentialError() {
 }
+
+type NoSuchUserError interface {
+	error
+	noSuchUserError()
+}
+
+func newNoSuchUserError() NoSuchUserError {
+	return new(noSuchUserError)
+}
+
+func IsNoSuchUserError(err error) bool {
+	_, ok := err.(NoSuchUserError)
+	return ok
+}
+
+type noSuchUserError struct {
+}
+
+func (e noSuchUserError) Error() string {
+	return "no such user"
+}
+
+func (e noSuchUserError) noSuchUserError() {
+}
