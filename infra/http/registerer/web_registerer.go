74e56e2b800ec015f45e60d3c5860455d189bbb9
tomocy <tomocy.dev@gmail.com>
2019-05-02 23:40:43 +0900

    guard routes with RenewInvalidSession

--- infra/http/registerer/web_registerer.go
+++ infra/http/registerer/web_registerer.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/tomocy/archs/infra/http/handler/web"
+	"github.com/tomocy/archs/infra/http/middleware"
 	"github.com/tomocy/archs/infra/http/route"
 )
 
@@ -19,6 +20,7 @@ type WebRegisterer struct {
 }
 
 func (r *WebRegisterer) RegisterRoutes(router chi.Router) {
+	router.Use(middleware.RenewInvalidSession)
 	router.Get("/*", http.FileServer(http.Dir("resource/public")).ServeHTTP)
 	router.Get(route.Web.Route("user.new").Path, r.handler.ShowUserRegistrationForm)
 	router.Post(route.Web.Route("user.create").Path, r.handler.RegisterUser)
