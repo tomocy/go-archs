47ea14e6ca3d5d01160dbfafa7db1acba50ef12a
tomocy <tomocy.dev@gmail.com>
2019-05-02 15:16:51 +0900

    add Handler

--- infra/http/handler/web/handler.go
+++ infra/http/handler/web/handler.go
@@ -5,7 +5,24 @@ import (
 
 	"github.com/tomocy/archs/adapter/controller"
 	"github.com/tomocy/archs/adapter/presenter"
+	"github.com/tomocy/archs/usecase"
 )
+
+func New(
+	userUsecase usecase.UserUsecase,
+) *Handler {
+	return &Handler{
+		userHandler: newUserHandler(userUsecase),
+	}
+}
+
+type Handler struct {
+	userHandler *userHandler
+}
+
+func (h *Handler) RegisterUser(w http.ResponseWriter, r *http.Request) {
+	h.userHandler.registerUser(w, r)
+}
 
 func httpController(r *http.Request) *controller.HTTPController {
 	return controller.NewHTTPController(r)
