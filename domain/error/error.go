cc5d6790bc3b42b443fb625c9f1c9d2621fb842e
tomocy <tomocy.dev@gmail.com>
2019-05-02 19:41:50 +0900

    add domain/error

---
+++ domain/error/error.go
@@ -0,0 +1,65 @@
+package error
+
+import "fmt"
+
+func InInput(err error) bool {
+	input, ok := err.(input)
+	return ok && input.input()
+}
+
+type input interface {
+	input() bool
+}
+
+func InInternal(err error) bool {
+	internal, ok := err.(internal)
+	return ok && internal.internal()
+}
+
+type internal interface {
+	internal() bool
+}
+
+func NewValidationError(format string, a ...interface{}) *ValidationError {
+	err := &ValidationError{
+		baseError: newBaseError(statusInput, format, a...),
+	}
+
+	return err
+}
+
+type ValidationError struct {
+	*baseError
+}
+
+func newBaseError(status status, format string, a ...interface{}) *baseError {
+	return &baseError{
+		status: status,
+		msg:    fmt.Sprintf(format, a...),
+	}
+}
+
+type baseError struct {
+	status status
+	msg    string
+}
+
+func (e *baseError) input() bool {
+	return e.status == statusInput
+}
+
+func (e *baseError) internal() bool {
+	return e.status == statusInternal
+}
+
+func (e *baseError) Error() string {
+	return e.msg
+}
+
+type status int
+
+const (
+	_ status = iota
+	statusInput
+	statusInternal
+)
