88267ee288ec9b93c6c18c08c4219fe7f1bf5a87
tomocy <tomocy.dev@gmail.com>
2019-05-02 19:54:57 +0900

    just wrap error

--- usecase/user_usecase.go
+++ usecase/user_usecase.go
@@ -6,7 +6,6 @@ import (
 	"github.com/pkg/errors"
 	"github.com/tomocy/archs/domain/repository"
 	"github.com/tomocy/archs/domain/service"
-	uerr "github.com/tomocy/archs/usecase/error"
 	"github.com/tomocy/archs/usecase/input"
 	"github.com/tomocy/archs/usecase/output"
 )
@@ -34,30 +33,20 @@ type userUsecase struct {
 func (u *userUsecase) registerUser(input input.RegisterUserInput, output output.RegisterUserOutput) {
 	user := input.ToRegisterUser()
 	if err := user.AllocateID(u.repo.NextUserID()); err != nil {
-		output.OnError(
-			wrapValidationError(uerr.KindUserRegistration, "register user", err),
-		)
+		output.OnError(wrapError(err, "register user"))
 		return
 	}
 	if err := user.HashPassword(u.hashServ); err != nil {
-		// TODO: switch on error
-		output.OnError(
-			wrapValidationError(uerr.KindUserRegistration, "register user", err),
-		)
+		output.OnError(wrapError(err, "register user"))
 		return
 	}
 	if err := user.ValidateSelf(); err != nil {
-		output.OnError(
-			wrapValidationError(uerr.KindUserRegistration, "register user", err),
-		)
+		output.OnError(wrapError(err, "register user"))
 		return
 	}
 
 	if err := u.repo.SaveUser(user); err != nil {
-		// TODO: switch on error
-		output.OnError(
-			wrapValidationError(uerr.KindUserRegistration, "register user", err),
-		)
+		output.OnError(wrapError(err, "register user"))
 		return
 	}
 
@@ -68,19 +57,13 @@ func (u *userUsecase) findUser(input input.FindUserInput, output output.FindUser
 	id := input.ToFindUser()
 	user, err := u.repo.FindUser(id)
 	if err != nil {
-		// TODO: switch on error
-		output.OnError(
-			wrapValidationError(uerr.KindUserRegistration, "find user", err),
-		)
+		output.OnError(wrapError(err, "find user"))
 		return
 	}
 
 	output.OnUserFound(user)
 }
 
-func wrapValidationError(kind uerr.Kind, did string, err error) error {
-	return errors.Wrap(
-		uerr.NewValidationError(kind, err.Error()),
-		fmt.Sprintf("failed to %s", did),
-	)
+func wrapError(err error, did string) error {
+	return errors.Wrap(err, fmt.Sprintf("failed to %s", did))
 }
