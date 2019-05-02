16afdab3e41da396a91690165210c6a5002ab77c
tomocy <tomocy.dev@gmail.com>
2019-05-02 16:50:48 +0900

    add ShowUserRegistrationForm to Handler

--- infra/http/handler/web/handler.go
+++ infra/http/handler/web/handler.go
@@ -1,23 +1,31 @@
 package web
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/tomocy/archs/adapter/controller"
 	"github.com/tomocy/archs/adapter/presenter"
-	"github.com/tomocy/archs/usecase"
+	"github.com/tomocy/archs/infra/http/view"
 )
 
 func New(
-	userUsecase usecase.UserUsecase,
+	view view.View,
 ) *Handler {
 	return &Handler{
-		userHandler: newUserHandler(userUsecase),
+		view: view,
 	}
 }
 
 type Handler struct {
+	view        view.View
 	userHandler *userHandler
+}
+
+func (h *Handler) ShowUserRegistrationForm(w http.ResponseWriter, r *http.Request) {
+	if err := h.view.Show(w, "user.new", nil); err != nil {
+		logInternalServerError(w, "show user registration form", err)
+	}
 }
 
 func (h *Handler) RegisterUser(w http.ResponseWriter, r *http.Request) {
@@ -30,4 +38,9 @@ func httpController(r *http.Request) *controller.HTTPController {
 
 func httpPresenter(w http.ResponseWriter, r *http.Request) *presenter.HTTPPresenter {
 	return presenter.NewHTTPPresenter(w, r)
+}
+
+func logInternalServerError(w http.ResponseWriter, did string, msg interface{}) {
+	log.Printf("failed to %s: %v\n", did, msg)
+	w.WriteHeader(http.StatusInternalServerError)
 }
