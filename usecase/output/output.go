1470ca4aa195e5cc0c39bdc3574545b1906a90a7
tomocy <tomocy.dev@gmail.com>
2019-05-02 15:12:03 +0900

    add RegisterUserOutput

--- usecase/output/output.go
+++ usecase/output/output.go
@@ -1,5 +1,12 @@
 package output
 
+import "github.com/tomocy/archs/domain/model"
+
 type UsecaseOutput interface {
 	OnError(err error)
+}
+
+type RegisterUserOutput interface {
+	UsecaseOutput
+	OnUserRegistered(user *model.User)
 }
