ccf7132a9387e27672a2e34a7ca09823436a4c69
tomocy <tomocy.dev@gmail.com>
2019-05-02 16:38:19 +0900

    add route

---
+++ infra/http/route/route.go
@@ -0,0 +1,37 @@
+package route
+
+import (
+	"fmt"
+	"path"
+
+	"github.com/tomocy/route"
+)
+
+func MapRoutes(addr string) {
+	mapRoutes(Web, webRaw, addr)
+}
+
+var (
+	Web = make(route.RouteMap)
+)
+
+var (
+	webRaw = route.RawMap{
+		"user.new": "/users/new",
+	}
+)
+
+func mapRoutes(routeMap route.RouteMap, rawMap route.RawMap, addr string) {
+	makeURLAbsolute(rawMap, addr)
+	routeMap.Map(rawMap)
+}
+
+func makeURLAbsolute(rmap route.RawMap, addr string) {
+	for key := range rmap {
+		rmap[key] = rawAbsoluteURL(addr, rmap[key])
+	}
+}
+
+func rawAbsoluteURL(addr, p string) string {
+	return fmt.Sprintf("%s://%s%s", "http", addr, path.Join("/", p))
+}
